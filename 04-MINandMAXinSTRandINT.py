9c65aeeb06f03e7930ceeedb08fdc54ffeb227a6
Mehmet Ugur <mehmetbugur@hotmail.com>
2023-02-05 11:44:07 +0800

    add 04-MINandMAXinSTRandINT.py

---
+++ 04-MINandMAXinSTRandINT.py
@@ -0,0 +1,23 @@
+varint = [1, 4, -5, 16, -89, 398]
+varstr1 = "Hello world"
+varstr2 = "Hello World"
+varintmin = min(varint)
+varintmax = max(varint)
+varstr1min = min(varstr1)
+varstr1max = max(varstr1)
+varstr2min = min(varstr2)
+varstr2max = max(varstr2)
+
+print (f'''
+Int list is       : {varint}
+min number        = {varintmin}
+max number        = {varintmax}
+*************************
+Str No.1 is       : {varstr1}
+Smallest letter is: {varstr1min}
+Biggest letter is : {varstr1max}
+{'*'*25}
+Str No.2 is       : {varstr2}
+Smallest letter is: {varstr2min}
+Biggest letter is : {varstr2max}
+''')
